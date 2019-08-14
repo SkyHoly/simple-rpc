126a292fc89f9cdbf5960cdcee96f55e90c694d6
SkyHoly <404693048@qq.com>
2019-08-14 23:50:38 +0800

    test version

---
+++ src/main/java/com/jwfy/simplerpc/my_v1/MethodParameter.java
@@ -0,0 +1,49 @@
+package com.jwfy.simplerpc.my_v1;
+
+/**
+ * @author zsj
+ * @date 2019/8/14
+ */
+public class MethodParameter {
+
+    private String className;
+
+    private String methodName;
+
+    private Object[] arguments;
+
+    private Class<?>[] parameterTypes;
+
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public Object[] getArguments() {
+        return arguments;
+    }
+
+    public void setArguments(Object[] arguments) {
+        this.arguments = arguments;
+    }
+
+    public Class<?>[] getParameterTypes() {
+        return parameterTypes;
+    }
+
+    public void setParameterTypes(Class<?>[] parameterTypes) {
+        this.parameterTypes = parameterTypes;
+    }
+}
